bf0cfa1e3885901c345c971809caf2f731d87e0b
Muhammad Imran <imran.sahadvi@gmail.com>
2024-05-24 10:10:20 +0500

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
     return (
         <Layout title={"Contact Us - SadaBahar"}>
             <div className='flex items-center justify-center gap-3 md:flex-row flex-col'>
-                <img className=' md:w-2/5 md:m-5 m-3 select-none' src="/images/contact.svg" alt="contact us" />
+                <img className=' md:w-2/5 md:m-5 m-3 select-none' src="https://img.freepik.com/free-vector/flat-design-illustration-customer-support_23-2148887720.jpg?w=740&t=st=1716527330~exp=1716527930~hmac=8f3d774016e5181a15dc92eeb1cba165d42355788aa74d8e26d74cfc9880d35e" alt="contact us" />
                 <div className='md:w-4/12 mx-10 mb-24'>
                     <div className='bg-indigo-500 text-center md:pb-4 md:pt-3 md:mb-3 text-white md:text-5xl text-4xl'>contact us</div>
                     <p className='text-lg pt-6'>any query and info about product feel free to call any time. we 24x7 available</p>
